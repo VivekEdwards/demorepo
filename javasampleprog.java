02305073771dc9496ab364b26d233125b828aa25
vivekedwards <m.v.edwards30@gmail.com>
2023-02-27 11:30:49 +0000

    code correction

--- javasampleprog.java
+++ javasampleprog.java
@@ -3,7 +3,7 @@ class javasampleprog
 {
 	public static void main(String arg[])
 	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(system.in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter you name:");
 		String name = br.readLine();
 		System.out.println("Hello!! "+name+" Welcome to GitHub and this prog written in java");
